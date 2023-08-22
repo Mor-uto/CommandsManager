e6664d09beb1432d332d6272e13a9523defd954c
Moruto <79018613+Mor-uto@users.noreply.github.com>
2023-08-22 04:29:15 +0300

    Create Command.java

---
+++ src/Command.java
@@ -0,0 +1,22 @@
+public abstract class Command implements CommandExecutor {
+    public String name;
+
+    public Command() {
+        this.name = getClass().getAnnotation(CommandInfo.class).name();
+    }
+
+    public abstract void execute(Player player, String[] args);
+
+    public void sendMessageBranded(Player player, String message) {
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        if (sender instanceof Player player) {
+            execute(player, args);
+        }
+
+        return true;
+    }
+}
